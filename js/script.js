48b3c768d62b1c7c560dfaeb327ce13a3d88ffb1
Sneha harigharun <109866539+sneha2001@users.noreply.github.com>
2024-03-13 19:21:59 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*-----------typing animation-------------*/
+var typed = new Typed(".typing",{
+    strings : ["Full stack Developer","Web Designer", "Web Developer", "Freelancer",],
+    typeSpeed : 100,
+    Backspeed:60,
+    loop: true
+})
